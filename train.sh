b910a95c144fc906782d645af4cef51c7416ab4b
sungho.park <sungho.park@webtoonscorp.com>
2023-03-16 14:06:02 +0000

    add

--- train.sh
+++ train.sh
@@ -1,9 +1,9 @@
 python train.py \
-    --model_name_or_path decapoda-research/llama-7b-hf \
+    --model_name_or_path $model_path \
     --dataset_name alpaca_data.hf \
     --is_dataset_from_disk True \
     --per_device_train_batch_size 8 \
     --per_device_eval_batch_size 8 \
     --do_train \
     --do_eval \
-    --output_dir test-clm
+    --output_dir $output_dir
